8b12d290acd85ff0fc81527c327225fca8514bdc
Dinesh Katamneni <31161716+dineshkatamneni@users.noreply.github.com>
2021-09-27 00:52:42 -0300

    Delete accountInformation directory

--- accountInformation/IAccountClosure.java
+++
@@ -1,8 +0,0 @@
-package com.wayne.project.accountInformation;
-
-import java.sql.SQLException;
-
-public interface IAccountClosure {
-
-    public boolean closeAccount(String choice) throws SQLException;
-}
--- accountInformation/IRequestChequeBook.java
+++
@@ -1,8 +0,0 @@
-package com.wayne.project.accountInformation;
-
-import java.sql.SQLException;
-
-public interface IRequestChequeBook {
-
-    public String requestChequeBook(String accountType, String choice) throws SQLException;
-}
