7ca401cacb229ac479bfe4fd314305d7bd5a6b6d
Dinesh Katamneni <31161716+dineshkatamneni@users.noreply.github.com>
2021-09-27 00:29:45 -0300

    Add files via upload

---
+++ src/consoleDriver/ProjectApplication.java
@@ -0,0 +1,11 @@
+package consoleDriver;
+
+import java.io.IOException;
+
+public class ProjectApplication {
+
+	public static void main(String[] args) throws IOException {
+		ConsoleRunner Runner = new ConsoleRunner();
+		Runner.run();
+	}
+}
